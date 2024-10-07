27d9f5a47de44e874cb2f50618acef04c17fb583
jinchow <983412279@qq.com>
2024-10-07 21:31:39 +0800

    Add files via upload

---
+++ mmaction/models/backbones/vit_clip.py
@@ -0,0 +1,215 @@
+import math
+from collections import OrderedDict
+from typing import Tuple, Union
+from timm.models.layers import DropPath, to_2tuple, trunc_normal_
+import numpy as np
+import torch
+import torch.nn.functional as F
+from torch import nn
+import clip
+from mmaction.utils import get_root_logger
+from einops import rearrange
+from ..builder import BACKBONES
+
+
+class STAdapter(nn.Module):
+
+    def __init__(self, in_channels=768, adapter_channels=384):
+        super().__init__()
+        self.fc1 = nn.Linear(in_channels, adapter_channels)
+        self.conv = nn.Conv3d(
+            adapter_channels, adapter_channels,
+            kernel_size=(3, 1, 1),
+            stride=(1, 1, 1),
+            padding=tuple([1, 0, 0]),
+            groups=adapter_channels,
+        )
+        self.fc2 = nn.Linear(adapter_channels, in_channels)
+        nn.init.constant_(self.conv.weight, 0.)
+        nn.init.constant_(self.conv.bias, 0.)
+        nn.init.constant_(self.fc1.bias, 0.)
+        nn.init.constant_(self.fc2.bias, 0.)
+
+    def forward(self, x, T):
+        BT, L, C = x.size()
+        B = BT // T
+        Ca = self.conv.in_channels
+        H = W = round(math.sqrt(L - 1))
+        assert L - 1 == H * W
+        x_id = x
+        x = x[:, 1:, :]
+        x = self.fc1(x)
+        x = x.view(B, T, H, W, Ca).permute(0, 4, 1, 2, 3).contiguous()
+
+        # Whether cuDNN should be temporarily disable for 3D depthwise convolution.
+        # For some PyTorch builds the built-in 3D depthwise convolution may be much
+        # faster than the cuDNN implementation. You may experiment with your specific
+        # environment to find out the optimal option.
+        DWCONV3D_DISABLE_CUDNN = True
+        cudnn_enabled = torch.backends.cudnn.enabled
+        torch.backends.cudnn.enabled = cudnn_enabled and DWCONV3D_DISABLE_CUDNN
+        x = self.conv(x)
+        torch.backends.cudnn.enabled = cudnn_enabled
+
+        x = x.permute(0, 2, 3, 4, 1).contiguous().view(BT, L - 1, Ca)
+        x = self.fc2(x)
+        x_id[:, 1:, :] += x
+        return x_id
+
+
+class LayerNorm(nn.LayerNorm):
+    """Subclass torch's LayerNorm to handle fp16."""
+
+    def forward(self, x: torch.Tensor):
+        orig_type = x.dtype
+        ret = super().forward(x.type(torch.float32))
+        return ret.type(orig_type)
+
+
+class QuickGELU(nn.Module):
+    def forward(self, x: torch.Tensor):
+        return x * torch.sigmoid(1.702 * x)
+
+
+class ResidualAttentionBlock(nn.Module):
+    def __init__(self, d_model: int, n_head: int, attn_mask: torch.Tensor = None, scale=1., num_tadapter=1,
+                 num_frames=8, drop_path=0.):
+        super().__init__()
+        self.num_tadapter = num_tadapter
+        self.attn = nn.MultiheadAttention(d_model, n_head)
+        self.ln_1 = LayerNorm(d_model)
+        self.mlp = nn.Sequential(OrderedDict([
+            ("c_fc", nn.Linear(d_model, d_model * 4)),
+            ("gelu", QuickGELU()),
+            ("c_proj", nn.Linear(d_model * 4, d_model))
+        ]))
+        self.ln_2 = LayerNorm(d_model)
+        self.attn_mask = attn_mask
+        self.n_head = n_head
+        self.scale = scale
+        self.num_frames = num_frames
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+
+    def attention(self, x: torch.Tensor):
+        self.attn_mask = self.attn_mask.to(dtype=x.dtype, device=x.device) if self.attn_mask is not None else None
+        return self.attn(x, x, x, need_weights=False, attn_mask=self.attn_mask)[0]
+
+    def forward(self, x: torch.Tensor):
+        ## x shape [HW+1, BT, D]
+        n, bt, d = x.shape
+        ## x shape [BT, HW+1, D]
+        x = x + self.attention(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
+
+
+class Transformer(nn.Module):
+    def __init__(self, num_frames, width: int, layers: int, heads: int, attn_mask: torch.Tensor = None, num_tadapter=1,
+                 scale=1., drop_path=0.1):
+        super().__init__()
+        self.width = width
+        self.layers = layers
+        dpr = [x.item() for x in torch.linspace(0, drop_path, self.layers)]
+        self.resblocks = nn.Sequential(
+            *[ResidualAttentionBlock(width, heads, attn_mask, scale, num_tadapter, num_frames, dpr[i]) for i in
+              range(layers)])
+
+    def forward(self, x: torch.Tensor):
+        return self.resblocks(x)
+
+
+@BACKBONES.register_module()
+class ViT_CLIP(nn.Module):
+    ## ViT definition in CLIP image encoder
+    def __init__(self, input_resolution: int, num_frames: int, patch_size: int, width: int, layers: int, heads: int,
+                 drop_path_rate, num_tadapter=1, adapter_scale=0.5, pretrained=None):
+        super().__init__()
+        self.input_resolution = input_resolution
+        self.pretrained = pretrained
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=width, kernel_size=patch_size, stride=patch_size, bias=False)
+
+        scale = width ** -0.5
+        self.layers = layers
+        self.class_embedding = nn.Parameter(scale * torch.randn(width))
+        self.positional_embedding = nn.Parameter(scale * torch.randn((input_resolution // patch_size) ** 2 + 1, width))
+        self.ln_pre = LayerNorm(width)
+
+        self.num_frames = num_frames
+        self.temporal_embedding = nn.Parameter(torch.zeros(1, num_frames, width))
+
+        self.transformer = Transformer(num_frames, width, layers, heads, num_tadapter=num_tadapter, scale=adapter_scale,
+                                       drop_path=drop_path_rate)
+
+        self.ln_post = LayerNorm(width)
+
+    def init_weights(self, pretrained=None):
+        def _init_weights(m):
+            if isinstance(m, nn.Linear):
+                trunc_normal_(m.weight, std=.02)
+                if isinstance(m, nn.Linear) and m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.LayerNorm):
+                nn.init.constant_(m.bias, 0)
+                nn.init.constant_(m.weight, 1.0)
+
+        if pretrained:
+            self.pretrained = pretrained
+        if isinstance(self.pretrained, str):
+            self.apply(_init_weights)
+            logger = get_root_logger()
+            logger.info(f'load model from: {self.pretrained}')
+            ## Load OpenAI CLIP pretrained weights
+            if self.layers == 12:
+                clip_model, preprocess = clip.load("ViT-B/16", device="cpu")
+            else:
+                clip_model, preprocess = clip.load("ViT-L/14", device="cpu")
+            pretrain_dict = clip_model.visual.state_dict()
+            del clip_model
+            del pretrain_dict['proj']
+            msg = self.load_state_dict(pretrain_dict, strict=False)
+            logger.info('Missing keys: {}'.format(msg.missing_keys))
+            logger.info('Unexpected keys: {}'.format(msg.unexpected_keys))
+            logger.info(f"=> loaded successfully '{self.pretrained}'")
+            torch.cuda.empty_cache()
+        elif self.pretrained is None:
+            self.apply(_init_weights)
+        else:
+            raise TypeError('pretrained must be a str or None')
+
+
+    @torch.jit.ignore
+    def no_weight_decay(self):
+        return {'absolute_pos_embed', 'temporal_embedding'}
+
+    @torch.jit.ignore
+    def no_weight_decay_keywords(self):
+        return {'relative_position_bias_table', 'temporal_position_bias_table'}
+
+    def forward(self, x: torch.Tensor):
+        B, C, T, H, W = x.shape
+        x = rearrange(x, 'b c t h w -> (b t) c h w')
+        x = self.conv1(x)
+        x = x.reshape(x.shape[0], x.shape[1], -1)
+        x = x.permute(0, 2, 1)
+        x = torch.cat(
+            [self.class_embedding.to(x.dtype) + torch.zeros(x.shape[0], 1, x.shape[-1], dtype=x.dtype, device=x.device),
+             x], dim=1)
+        x = x + self.positional_embedding.to(x.dtype)
+
+        n = x.shape[1]
+        x = rearrange(x, '(b t) n d -> (b n) t d', t=self.num_frames)
+        x = x + self.temporal_embedding
+        x = rearrange(x, '(b n) t d -> (b t) n d', n=n)
+
+        x = self.ln_pre(x)
+
+        x = x.permute(1, 0, 2)  # NLD -> LND
+        x = self.transformer(x)
+        x = x.permute(1, 0, 2)  # LND -> NLD
+        x = self.ln_post(x)
+        x = x[:, 0]
+        x = rearrange(x, '(b t) d -> b d t', b=B, t=T)
+
+        x = x.unsqueeze(-1).unsqueeze(-1)  # BDTHW for I3D head
+
+        return x
